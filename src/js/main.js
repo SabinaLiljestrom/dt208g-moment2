b33d52c88381b3c0435fbab641ac68049dc547af
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-04-12 11:16:50 +0200

    har gjort om checkbox för att markera uppgift som klar till en knapp istället och lagt till en eventlyssnare som aktiveras på klick och strycke över texten

--- src/js/main.js
+++ src/js/main.js
@@ -3,25 +3,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM is fully loaded and parsed');
 });
-/*funktion skriva ut todo på sidan */
+//funktion skriva ut todo på sidan //
 function printTodoDetails(todo) {
     var todoListDiv = document.getElementById("todoList");
     if (todoListDiv) {
         var newTodoDiv = document.createElement("div");
-        newTodoDiv.innerHTML = "\n            <p><strong>Att g\u00F6ra:</strong> <span>".concat(todo.task, "</span></p>\n            <p><strong>Prioritet:</strong> <span>").concat(todo.priority, "</span></p>\n            <p><strong>Markera som klar:</strong> <input type=\"checkbox\" ").concat(todo.completed ? 'checked' : '', " disabled></p>\n        ");
+        newTodoDiv.innerHTML = "\n        <p><span id=\"taskSpan\" contenteditable=\"true\" data-key=\"task\">".concat(todo.task, "</span></p>\n        <p>Prioritet: <span id=\"prioritySpan\" contenteditable=\"true\" data-key=\"priority\">").concat(todo.priority, "</span></p>\n        <button id=\"completedButton\" ").concat(todo.completed ? 'style="text-decoration: line-through;"' : '', " ").concat(todo.completed ? 'disabled' : '', ">Markera som klar</button>\n        ");
         todoListDiv.appendChild(newTodoDiv);
     }
 }
+//hämta DOM-element för formulär och todo detaljer
 var todoForm = document.getElementById("todoForm");
 var todos = JSON.parse(localStorage.getItem("todos") || "[]");
+//skriva ut todo detaljer för varje sparade todo från local storage när sidan laddas om
 todos.forEach(function (todo) {
     printTodoDetails(todo);
 });
-/*eventlyssnar på lägg till knappen*/
+//eventlyssnar på lägg till knappen
 todoForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    //hämta värden som skirvs in i förmuläret
     var taskInput = document.getElementById("task");
     var priorityInput = document.getElementById("priority");
+    //skapar todo objekt och sparar i lokal array och localstorage 
     var newTodo = {
         task: taskInput.value,
         priority: parseInt(priorityInput.value),
@@ -32,4 +36,15 @@ todoForm.addEventListener("submit", function (event) {
     printTodoDetails(newTodo);
     taskInput.value = ''; // Rensar input efter att man lagt till 
     priorityInput.value = ''; // Rensar prioritet
+});
+//eventlyssnare klar knapp, texten strycks över när den är gjord
+document.addEventListener('click', function (event) {
+    var _a;
+    if (event.target && event.target.id === 'completedButton') {
+        var taskSpan = (_a = event.target.closest('div')) === null || _a === void 0 ? void 0 : _a.querySelector('#taskSpan');
+        if (taskSpan) {
+            taskSpan.style.textDecoration = 'line-through';
+        }
+        event.target.disabled = true;
+    }
 });
--- src/js/main.ts
+++ src/js/main.ts
@@ -3,35 +3,35 @@ import { ITodo } from './ITodo';
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM is fully loaded and parsed');
 });
-/*funktion skriva ut todo på sidan */
+//funktion skriva ut todo på sidan //
 function printTodoDetails(todo: ITodo): void {
     const todoListDiv = document.getElementById("todoList");
 
     if (todoListDiv) {
         const newTodoDiv = document.createElement("div");
         newTodoDiv.innerHTML = `
-            <p><strong>Att göra:</strong> <span>${todo.task}</span></p>
-            <p><strong>Prioritet:</strong> <span>${todo.priority}</span></p>
-            <p><strong>Markera som klar:</strong> <input type="checkbox" ${todo.completed ? 'checked' : ''} disabled></p>
+        <p><span id="taskSpan" contenteditable="true" data-key="task">${todo.task}</span></p>
+        <p>Prioritet: <span id="prioritySpan" contenteditable="true" data-key="priority">${todo.priority}</span></p>
+        <button id="completedButton" ${todo.completed ? 'style="text-decoration: line-through;"' : ''} ${todo.completed ? 'disabled' : ''}>Markera som klar</button>
         `;
         todoListDiv.appendChild(newTodoDiv);
     }
 }
-
+//hämta DOM-element för formulär och todo detaljer
 const todoForm = document.getElementById("todoForm") as HTMLFormElement;
 let todos: ITodo[] = JSON.parse(localStorage.getItem("todos") || "[]");
-
+//skriva ut todo detaljer för varje sparade todo från local storage när sidan laddas om
 todos.forEach(todo => {
     printTodoDetails(todo);
 });
 
-/*eventlyssnar på lägg till knappen*/
+//eventlyssnar på lägg till knappen
 todoForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
+//hämta värden som skirvs in i förmuläret
     const taskInput = document.getElementById("task") as HTMLInputElement;
     const priorityInput = document.getElementById("priority") as HTMLSelectElement;
-    
+    //skapar todo objekt och sparar i lokal array och localstorage 
     const newTodo: ITodo = {
         task: taskInput.value,
         priority: parseInt(priorityInput.value),
@@ -44,4 +44,14 @@ todoForm.addEventListener("submit", (event) => {
 
     taskInput.value = ''; // Rensar input efter att man lagt till 
     priorityInput.value = ''; // Rensar prioritet
+});
+//eventlyssnare klar knapp, texten strycks över när den är gjord
+document.addEventListener('click', (event) => {
+    if (event.target && (event.target as HTMLElement).id === 'completedButton') {
+        const taskSpan = (event.target as HTMLElement).closest('div')?.querySelector('#taskSpan') as HTMLElement;
+        if (taskSpan) {
+            taskSpan.style.textDecoration = 'line-through';
+        }
+        (event.target as HTMLButtonElement).disabled = true;
+    }
 });
